3aa8c99e6c39347891e2388f05929979397348bc
hemadri-git <bathulahemadri@gmail.com>
2024-09-23 14:45:10 +0530

    day1

--- JavaPrograms/src/com/programs/basic/TvSellAndProfit.java
+++ JavaPrograms/src/com/programs/basic/TvSellAndProfit.java
@@ -4,8 +4,8 @@ public class TvSellAndProfit {
 	public static void main(String[] args) {
 		double cost = 32500;
 		double profit = cost * 0.27;
-		double vat = 12.7/100;
-		double service = 3.87/100;
+		double vat = 12.7 / 100;
+		double service = 3.87 / 100;
 		double sellingPrice = cost + profit;
 		double vatTax = sellingPrice * vat;
 		double serviceCharge = sellingPrice * service;
@@ -14,7 +14,7 @@ public class TvSellAndProfit {
 		System.out.println("Profit : " + profit);
 		System.out.println("Vat : " + vatTax);
 		System.out.println("service charge: " + serviceCharge);
-				
+
 	}
 
 }
