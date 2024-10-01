febe6cdf27a7de6307b419b845e0c3019f9da288
hemadri-git <bathulahemadri@gmail.com>
2024-10-01 22:28:20 +0530

    BLC & ELC

--- JavaPrograms/src/com/programs/resuable_code_demo/NextMultipleOfHundredTest.java
+++ JavaPrograms/src/com/programs/resuable_code_demo/NextMultipleOfHundredTest.java
@@ -1,3 +1,16 @@
+/*Program-3
+Define a method which returns the next multiple of 100 for the given number.
+Write the method with the following specifications: 
+Name of the BLC class:NextMultipleOfHundred
+Name of method: getNextMultipleOfHundred()
+Access Modifier : public, static
+Arguments: one argument of type integer
+Return type: an integer value
+For example,
+If the given value is 123, return 200.
+
+Create an ELC class Main to test the application*/
+
 package com.programs.resuable_code_demo;
 
 public class NextMultipleOfHundredTest {
